8d8536973910b34e53a5a627e21705e98141c741
Jan Willem van Jaarsveld <janwvjaarsveld@gmail.com>
2022-12-17 14:52:36 +0100

    testing fugitive

--- nvim/lua/jw/packer.lua
+++ nvim/lua/jw/packer.lua
@@ -2,21 +2,7 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use('wbthomason/packer.nvim')
 
-  use('tomasr/molokai')
-
-  use('nvim-lua/plenary.nvim')
-
-  use({ 'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  })
-
-  use('nvim-treesitter/nvim-treesitter', {
-    run = ':TSUpdate'
-  })
-
-  use('ThePrimeagen/git-worktree.nvim')
-
-  use('romgrk/nvim-treesitter-context')
+  -- use('tomasr/molokai')
 
   use({ 'numToStr/Comment.nvim',
     config = function()
@@ -24,96 +10,48 @@ return require('packer').startup(function(use)
     end
   })
 
-  use({ 'nvim-tree/nvim-tree.lua',
-    requires = { 'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    }, tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  })
-
-  use("mbbill/undotree")
-
   use({
     "folke/which-key.nvim",
     config = function()
-      require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
+      require("which-key").setup {}
     end
   })
 
-  use("williamboman/mason.nvim")
-  use({
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓"
-          }
-        }
-      })
-      require("mason-lspconfig").setup_handlers({
-        ["pylsp"] = function()
-          require("lspconfig").pylsp.setup({
-            on_attach = function(client, bufnr)
-              require("navigator.lspclient.mapping").setup({ client = client, bufnr = bufnr }) -- setup navigator keymaps here,
-              require("navigator.dochighlight").documentHighlight(bufnr)
-              require("navigator.codeAction").code_action_prompt(bufnr)
-            end,
-          })
-        end,
-      })
-      require("mason-lspconfig").setup({
-        ensure_installed = { "sumneko_lua", "rust_analyzer", "dockerls", "cssls", "eslint", "gopls", "graphql", "html",
-          "jsonls", "tsserver", "pyright", "terraformls", "yamlls" }
-      })
-    end,
-  })
-
-  use({
-    'ray-x/navigator.lua',
-    requires = {
-      { 'ray-x/guihua.lua', run = 'cd lua/fzy && make' },
-      { 'neovim/nvim-lspconfig' },
-      { 'ray-x/lsp_signature.nvim' },
-    },
-  })
-
-  use("simrat39/rust-tools.nvim")
-
-  use('jose-elias-alvarez/null-ls.nvim')
   use('MunifTanjim/prettier.nvim')
-  use("hrsh7th/cmp-nvim-lsp")
 
-  use({
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    opt = true,
-    config = function()
-      require("config.cmp").setup()
-    end,
-    wants = { "LuaSnip" },
-    requires = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lua",
-      "ray-x/cmp-treesitter",
-      "hrsh7th/cmp-cmdline",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-calc",
-      "f3fora/cmp-spell",
-      "hrsh7th/cmp-emoji",
-      {
-        "L3MON4D3/LuaSnip",
-        wants = "friendly-snippets",
-        config = function()
-          require("config.luasnip").setup()
-        end,
-      },
-      "rafamadriz/friendly-snippets",
-    },
-    disable = false,
+  use({ 'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
   })
-  use("nvim-lua/lsp_extensions.nvim")
+
+  use({ 'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function() vim.cmd('colorscheme rose-pine') end
+  })
+
+  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
+  use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+
+  use({ 'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
+  })
 end)
