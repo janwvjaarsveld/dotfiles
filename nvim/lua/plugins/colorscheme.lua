defddea4b33efdf9aa6c107365a8d195eb8e5bd0
Jan Willem van Jaarsveld <janwvjaarsveld@gmail.com>
2023-09-06 08:03:08 +0200

    add colorschemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
+-- material theme option
+vim.g.material_style = "deep ocean"
+
 return {
+  -- add OneDarker
+  { "lunarvim/Onedarker.nvim" },
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  -- add midnight
   { "dasupradyumna/midnight.nvim" },
+  -- add material
+  { "marko-cerovac/material.nvim" },
+  -- add embark
+  { "embark-theme/vim", as = "embark" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "midnight",
+      colorscheme = "material",
     },
   },
 }
