216ea66f4faa3660601ccb97714644f997dac626
janwvjaarsveld <janwvjaarsveld@gmail.com>
2024-12-18 20:22:52 +0100

    current state

--- nvim/lua/plugins/coding/neotest.lua
+++ nvim/lua/plugins/coding/neotest.lua
@@ -7,26 +7,29 @@ return {
     "nvim-neotest/neotest-go",
   },
   opts = {
+    discovery = {
+      enabled = true,
+    },
     adapters = {
       ["neotest-jest"] = {
-        jestCommand = "npx jest --",
+        jest_test_discovery = false,
+        jestCommand = "npx jest",
         jestConfigFile = function(file)
           if string.find(file, "/packages/") then
-            if string.find(file, "/src/") then
-              return string.match(file, "(.-/[^/]+/)src") .. "jest.config.js"
-            end
             return string.match(file, "(.-/[^/]+/)lib") .. "jest.config.ts"
           end
-
+          if string.find(file, "/src/") then
+            return string.match(file, "(.-/[^/]+/)src") .. "jest.config.js"
+          end
           return vim.fn.getcwd() .. "/jest.config.ts"
         end,
         env = { CI = true },
         cwd = function(file)
           if string.find(file, "/packages/") then
-            if string.find(file, "/src/") then
-              return string.match(file, "(.-/[^/]+/)src")
-            end
             return string.match(file, "(.-/[^/]+/)lib")
+          end
+          if string.find(file, "/src/") then
+            return string.match(file, "(.-/[^/]+/)src")
           end
           return vim.fn.getcwd()
         end,
--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -3,17 +3,63 @@ return {
   optional = true,
   opts = {
     formatters_by_ft = {
-      ["javascript"] = { "dprint", { "prettierd", "prettier" } },
-      ["javascriptreact"] = { "dprint" },
-      ["typescript"] = { "dprint", { "prettierd", "prettier" } },
-      ["typescriptreact"] = { "dprint" },
+      go = { "gopls", "goimports", "gofumpt", lsp_format = "fallback" },
+      json = { "prettierd" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      -- javascriptreact = { "dprint" },
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      rust = { "rustfmt", lsp_format = "fallback" },
+      typescript = { "prettierd", "prettier", lsp_format = "fallback", timeout_ms = 2500 },
+      -- typescriptreact = { "dprint" },
+      xml = { "xmlformatter" },
+      -- yml = { "prettierd", "prettier", lsp_format = "fallback" },
+      yaml = { "prettierd", "prettier", lsp_format = "fallback" },
     },
     formatters = {
-      dprint = {
-        condition = function(_, ctx)
-          return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-        end,
+      -- dprint = {
+      --   condition = function(_, ctx)
+      --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+      --   end,
+      -- },
+      prettier = {
+        command = "prettier",
+        root_patterns = {
+          ".prettierrc",
+          ".prettierrc.json",
+          ".prettierrc.yaml",
+          ".prettierrc.yml",
+          ".prettierrc.js",
+          ".prettierrc.cjs",
+          "prettier.config.js",
+          "prettier.config.cjs",
+        },
+      },
+      prettierd = {
+        command = "prettierd",
+        args = { "--stdin-filepath", "%filepath" },
+        root_patterns = {
+          ".prettierrc",
+          ".prettierrc.json",
+          ".prettierrc.yaml",
+          ".prettierrc.yml",
+          ".prettierrc.js",
+          ".prettierrc.cjs",
+          "prettier.config.js",
+          "prettier.config.cjs",
+        },
       },
     },
+    config = function(_, opts)
+      local conform = require("conform")
+
+      -- Setup "conform.nvim" to work
+      conform.setup(opts)
+
+      -- Customise the default "prettier" command to format Markdown files as well
+      conform.formatters.prettier = {
+        prepend_args = { "--prose-wrap", "always" },
+      }
+    end,
   },
 }
