55d8590e3c0a532b91560628d8e892f5a2ea459a
janwvjaarsveld <janwvjaarsveld@gmail.com>
2024-12-18 20:23:35 +0100

    fix: tmux sessions

--- tmux/sessions.sh
+++ tmux/sessions.sh
@@ -2,50 +2,57 @@ dir=${1:-$pwd}
 # session_name=${2:-"${PWD##*/}"}
 session_name=${2:-"default"}
 should_attach=${3:-true}
+name=${4}
 
-# we only want to list directories
-name="$(ls -l $dir | grep '^d' | awk '{print $9}' | fzf)"
-
+echo "$dir"
+echo "$session_name"
+echo "$should_attach"
+echo "$name"
 if [ -z "$name" ]; then
-	exit 0
+  # we only want to list directories
+  name="$(ls -l $dir | grep '^d' | awk '{print $9}' | fzf)"
+
+  if [ -z "$name" ]; then
+    exit 0
+  fi
 fi
 
 function new_session() {
-	tmux new-session -s $session_name -n $name -d
+  tmux new-session -s $session_name -n $name -d
 }
 
 function cd_and_nvim() {
-	tmux send-keys -t $session_name:$name 'cd ' $dir/$name ENTER 'clear && nvim' ENTER
+  tmux send-keys -t $session_name:$name 'cd ' $dir/$name ENTER 'clear && nvim' ENTER
 }
 
 function attach_session() {
-	tmux attach -t $session_name:$name
+  tmux attach -t $session_name:$name
 }
 
 if [ $(tmux ls 2>&1 | grep "no server running" | wc -l) -eq 1 ]; then
-	echo "Starting tmux..."
-	new_session
-	cd_and_nvim
-	if [ "$should_attach" = true ]; then
-		attach_session
-	fi
+  echo "Starting tmux..."
+  new_session
+  cd_and_nvim
+  if [ "$should_attach" = true ]; then
+    attach_session
+  fi
 elif [ $(tmux ls | grep $session_name | wc -l) -eq 1 ] && [ $(tmux list-windows -a | grep $name | wc -l) -eq 1 ]; then
-	echo "Session and Window exist, attaching..."
-	if [ "$should_attach" = true ]; then
-		attach_session
-	fi
+  echo "Session and Window exist, attaching..."
+  if [ "$should_attach" = true ]; then
+    attach_session
+  fi
 elif [ $(tmux ls | grep $session_name | wc -l) -eq 0 ]; then
-	echo "Session does not exist, creating..."
-	new_session
-	cd_and_nvim
-	if [ "$should_attach" = true ]; then
-		attach_session
-	fi
+  echo "Session does not exist, creating..."
+  new_session
+  cd_and_nvim
+  if [ "$should_attach" = true ]; then
+    attach_session
+  fi
 else
-	echo "Session exists, creating window..."
-	tmux new-window -n $name -t $session_name -d
-	cd_and_nvim
-	if [ "$should_attach" = true ]; then
-		attach_session
-	fi
+  echo "Session exists, creating window..."
+  tmux new-window -n $name -t $session_name -d
+  cd_and_nvim
+  if [ "$should_attach" = true ]; then
+    attach_session
+  fi
 fi
