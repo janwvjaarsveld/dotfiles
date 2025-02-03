a016a347b3144d4a1403a58ca142706039684b8b
janwvjaarsveld <janwvjaarsveld@gmail.com>
2025-02-03 23:50:53 +0100

    fix: add dracula theme

--- nvim/lua/custom/plugins/colorschema.lua
+++ nvim/lua/custom/plugins/colorschema.lua
@@ -1,8 +1,38 @@
 return {
+  -- {
   -- "catppuccin/nvim",
   -- name = "catppuccin",
   -- priority = 1000,
   -- config = function()
   --   vim.cmd("colorscheme catppuccin")
-  -- end,
+  -- end},
+  {
+    "maxmx03/dracula.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      require("dracula").setup({})
+      vim.cmd.colorscheme("dracula")
+    end,
+  },
+  -- {
+  --   "dracula/vim",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     -- load the colorscheme here
+  --     vim.cmd.colorscheme("dracula")
+  --   end,
+  -- },
+  -- {
+  --   "oxfist/night-owl.nvim",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     -- load the colorscheme here
+  --     require("night-owl").setup()
+  --     vim.cmd.colorscheme("night-owl")
+  --   end,
+  -- },
 }
