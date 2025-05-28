c60acee53d7a1b9da417c1a1653034aa177636fe
janwvjaarsveld <janwvjaarsveld@gmail.com>
2025-05-28 14:10:08 +0200

    chore: update conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      ["_"] = { "prettierd", "prettier", stop_after_first = true },
+      json = { "prettierd", "prettier", stop_after_first = true },
     },
   },
 }
